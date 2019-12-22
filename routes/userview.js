d4f61943b0167df25500a5e7af9c267e1c20848a
shaheemMPM <shaheem.mpm@gmail.com>
2019-12-22 15:30:11 +0530

    student view get qn

--- routes/userview.js
+++ routes/userview.js
@@ -1,7 +1,8 @@
 const express = require('express'),
       app     = express(),
       userFns = require('../functions/functions'),
-      passport    = require('passport')
+      passport    = require('passport'),
+      qnDb        = require('../models/questions')
 
 app.get('/login', userFns.isNotLoggedIn, (req, res) => {
   res.render('stdlogin')
@@ -18,7 +19,14 @@ app.get('/window', userFns.isUserLoggedIn, (req, res) => {
 })
 
 app.get('/qn/:id', userFns.isUserLoggedIn, (req, res) => {
-  res.render('examqn')
+  qnDb.find({qno: req.params.id}, '-__v -subject -option', (err, results) => {
+    if (err) {
+      console.log(`\n${new Date().toLocaleString()} : Error on registering student to database ::\n${err}\n`)
+      res.send("Server Error Contact Invigilator")
+    }else {
+      res.render('examqn', {data: results[0]})
+    }
+  })
 })
 
 app.get('/logout', userFns.isUserLoggedIn, (req, res) => {
